4ded10a82c385e8e1428a14869758ada7a2916a9
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-08-09 02:47:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Gunjan890/disha",
+    "https://github.com/Gunjan890/BrandrdXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
